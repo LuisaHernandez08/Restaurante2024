0e93b63b2c61872eef342c89dd632729056d473d
LuisaHernandez08 <160061181+LuisaHernandez08@users.noreply.github.com>
2024-04-01 17:38:52 -0500

    Creación del proyecto
    Incluye función de:
    1. Clientes
    2. Pedidos

---
+++ Restaurante/App_restaurante2/src/Datos/cls_pedidos.java
@@ -0,0 +1,22 @@
+package Datos;
+public class cls_pedidos {
+    private String id_clienteStr;
+    private String det_pedidoStr;
+    private float totalFlt;
+    
+    public cls_pedidos(String id, String detalle, float total){
+        this.det_pedidoStr = detalle;
+        this.id_clienteStr = id;
+        this.totalFlt = total;
+    }
+    
+    public String getDetalle(){
+        return this.det_pedidoStr;
+    }
+    public String getIdcliente(){
+        return this.id_clienteStr;
+    }
+    public float getTotal(){
+        return this.totalFlt;
+    }
+}
